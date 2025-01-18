1024b98bb7b62dce5da2a0c74a2b4c19e2bda150
Basile Dura <basile@bdura.me>
2025-01-19 00:21:49 +0100

    feat: add viewer with Nuxt & TresJS

---
+++ viewer/app.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+//import { STLLoader } from "three/addons/loaders/STLLoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+
+//const { scene } = await useLoader(
+//  GLTFLoader,
+//);
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresPerspectiveCamera :position="[3, 3, 3]" />
+    <OrbitControls />
+    <Box :scale="2">
+      <TresMeshToonMaterial color="orange" />
+    </Box>
+    <TresAmbientLight />
+    <TresDirectionalLight :position="[0, 2, 4]" />
+    <TresGridHelper />
+  </TresCanvas>
+</template>
